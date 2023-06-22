fa393f65d5558f38b6101bc813db18dcb2ba072a
Owen Jones <owenthefirst93@gmail.com>
2023-06-21 20:59:49 -0400

    checking git

--- client/src/Components/Header.js
+++ client/src/Components/Header.js
@@ -4,6 +4,7 @@ export const Header = () => {
     return (
         <div className='heading'>
             <h1>Owen's Weather App</h1>
+            <h5>Get weather data</h5>
         </div>
     )
 }
