31885f715025b7590596c84c7a02456da1f36404
ojones311 <owenthefirst93@gmail.com>
2023-06-27 16:32:09 -0400

    http request to weather api

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import './App.css';
 import {useState, useEffect} from 'react'
-import { useForm } from 'react-hook-form'
+// import { useForm } from 'react-hook-form'
 
 import { Header } from './Components/Header';
 import { InputCity } from './Components/InputCity';
@@ -10,10 +10,27 @@ function App() {
 
   const [inputCity, setInputCity] = useState('')
   const [cityName, setCityName] = useState('')
-  const { setError } = useForm()
+  const [weatherData, setWeatherData] = useState({});
+  const [ error, setError ] = useState(false)
+
+ 
   useEffect(()=> {
     //make api call to get weather data
-  }, [])
+    if (cityName !== '') {
+      fetch(`https://api.openweathermap.org/data/2.5/weather?q=${cityName}&APPID=${process.env.REACT_APP_WEATHER_API_KEY}`)
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data)
+        setWeatherData(data)
+      })
+      .catch((error)=> {
+        setError(true)
+        console.log(error.message)
+      })
+    }
+    
+  }, [cityName])
+
   const onInputHandler = (event) => {
     setInputCity(event.target.value)
     console.log(event.target.value)
@@ -34,7 +51,9 @@ function App() {
         onInputHandler={onInputHandler}
         onSubmitHandler={onSubmitHandler}
       />
-      <ShowWeather />
+      <ShowWeather 
+        weatherData={weatherData}
+      />
     </div>
   );
 }
