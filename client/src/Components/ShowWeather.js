50171e0b67a8e02cdca125699a38b1015439da33
ojones311 <owenthefirst93@gmail.com>
2023-06-28 11:43:04 -0400

    add temps and some style

--- client/src/Components/ShowWeather.js
+++ client/src/Components/ShowWeather.js
@@ -3,16 +3,22 @@ export const ShowWeather = ({data}) => {
 
     const city = data.name;
     const country = data.sys ? data.sys.country : null;
+    const icon = data.weather ? data.weather[0].icon : null;
     const descriptor = data.weather ? data.weather[0].main : null;
+
     const temperature = data.main ? data.main.temp : null;
+    const temp_min = data.main ? data.main.temp_min : null;
+    const temp_max = data.main ? data.main.temp_max : null;
+
     const pressure = data.main ? data.main.pressure : null;
     const visibility =  data ? data.visibility : null;
     const humidity = data.main ? data.main.humidity : null;
     const clouds =  data.clouds ? data.clouds.all : null;
-    const icon = data.weather ? data.weather[0].icon : null;
     const wind = data.wind ? data.wind.speed : null;
+
     const longitude = data.coord ? data.coord.lon : null;
     const latitude = data.coord ? data.coord.lat : null;
+
 
     const coordinateBearingMatcher = (measurement, degrees) => {
     
@@ -34,9 +40,21 @@ export const ShowWeather = ({data}) => {
         return coordinate
     }
 
-    const temperatureConverter = (temp) => {
-
+    const kelvinToFahrenheit = (temp) => {
+        //Fahrenheit equation (K − 273.15) × 9/5 + 32 = °F
+        if (temp){
+            temp = Math.round((temp - 273.15) * 9/5 + 32)
+        }
+        
+        return temp
     }
+
+    const kelvinToCelsius = (temp) => {
+        //Celsius equation C = K – 273.15
+        temp = Math.round(temp - 273.15)
+        return temp
+    }
+
     return (
         <div className='card-container'>
             <div className='weather'>
@@ -45,13 +63,21 @@ export const ShowWeather = ({data}) => {
                 <h3>{country}</h3>
                 <p>{coordinateBearingMatcher('latitude',latitude)}</p>
                 <p>{coordinateBearingMatcher('longitude',longitude)}</p>
-                <h4>{temperature}</h4>
+                <h3>{descriptor}</h3>
+                
+                {   temperature && 
+                    <div>
+                        <h4>{kelvinToFahrenheit(temperature)}{'°F'}</h4>
+                        <h4>{kelvinToCelsius(temperature)}{'°C'}</h4>
+                    </div>
+                }
             </div>
+            
             <div className='weather-icon'>
                 <table>
                     <tbody>
                         <tr>
-                            <td>{descriptor}</td>
+                            <td></td>
                             <td></td>
                         </tr>
                     </tbody>
