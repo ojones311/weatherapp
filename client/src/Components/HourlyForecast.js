733f6ad18e8c32489b3e0b1cc464425c299a7afb
ojones311 <owenthefirst93@gmail.com>
2023-08-24 20:59:53 -0400

    hourly forecast weather temps linechart props

--- client/src/Components/HourlyForecast.js
+++ client/src/Components/HourlyForecast.js
@@ -7,6 +7,7 @@ export const HourlyForecast = ({data}) => {
     const latitude = data.coord ? data.coord.lat : null;
 
     const [hourlyForecast, setHourlyForecast] = useState([])
+    const [hourlyTempsForLineChart, setHourlyTempsForLineChart] = useState([])
     const [ error, setError ] = useState(false)
 
     //data.dt_text
@@ -16,7 +17,8 @@ export const HourlyForecast = ({data}) => {
             .then((response) => response.json())
             .then((data)=> {
                 console.log(data)
-                setHourlyForecast(data)
+                setHourlyForecast(data.list)
+                console.log(data.list[0].main.temp)
             })
             .catch((error)=> {
                 setError(true)
@@ -25,6 +27,19 @@ export const HourlyForecast = ({data}) => {
         }
     }, [latitude,longitude])
 
+    useEffect(() => {
+        function extractHourlyForecastWeatherTemps(hourlyForecast){
+            const weatherTemps = []
+    
+            hourlyForecast.forEach(tempData => weatherTemps.push(tempData.main.temp))
+            console.log(weatherTemps)
+            setHourlyTempsForLineChart(weatherTemps)
+    
+            //need to make sure the function is called when the page is loaded but after the request is made to openweather api       //maybe a useeffect 
+        }
+        extractHourlyForecastWeatherTemps(hourlyForecast)
+    },[hourlyForecast])
+    
     
     return (
         <div>
@@ -34,10 +49,9 @@ export const HourlyForecast = ({data}) => {
                     <LineChart
                         width={400}
                         height={400}
-                        data={''}
+                        data={hourlyForecast}
                         margin={{ top: 5, right: 20, left: 10, bottom: 5 }}> 
-
-
+                        <Line type="monotone" dataKey="main" stroke="#8884d8" />
                     </LineChart>
                 }
                 
